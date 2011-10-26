9ce86adc7d2a57e60affb15986a9b1556017898a
Greg Borenstein <greg@mfdz.com>
2011-10-26 01:51:44 -0400

    cleanup

--- node/web.js
+++ node/web.js
@@ -1,54 +1,30 @@
 var net = require('net');
-
 var http = require('http');
+var io = require('socket.io').listen(5000);
+
 var gsock;
 
-// client
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   gsock.on("data", function(data) {
-//       console.log(data.toString());
-//       res.write(data.toString());
-//   })
-// }).listen(1338, "127.0.0.1");
-
-// server
 var server = net.createServer(function (socket) {
-//  socket.write("Echo server\r\n");
-        console.log("processing connected");
-
+  console.log("processing connected");
   socket.pipe(socket);
-  
   gsock = socket;
-  
-  var message = ""; // variable that collects chunks
-
-
-  
+    
   socket.on("data", function(data) {
-
-  
-
-      //console.log(data.toString());
   })
 });
 server.listen(1337, "127.0.0.1");
 
-var io = require('socket.io').listen(5000);
 
 io.sockets.on('connection', function (socket) {
-  //socket.emit('news', { hello: 'world' });
   console.log("browser connected");
   socket.emit('data', {message : "welcome browser"});
   
-  //if(gsock){
     
     var message = "";
     
     gsock.on("data", function(chunk) {
       
        message += chunk;
-       //console.log(message);
        
        try {
           json = JSON.parse(message); // let's not get crazy here
@@ -56,13 +32,10 @@ io.sockets.on('connection', function (socket) {
           socket.emit("data", json);
           
        } catch (Err) {
-           //console.log(message);
            console.log("skipping: " + Err);
            return; // continue
        }
-                      message = "";
+       message = "";
 
-      //console.log(data.toString());
     });
-  //}
 });
